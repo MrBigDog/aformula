ac0ab058549e5dab0436d2a8f2151493416a2998
Charles Pence <charles@charlespence.net>
2018-10-29 13:23:11 +0100

    Early quick build changes for new compilers with the libjit backend.

--- src/libjitformula.cpp
+++ src/libjitformula.cpp
@@ -1,16 +1,16 @@
 /*
   Copyright (C) 2010-2011  Charles Pence <charles@charlespence.net>
-  
+
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -75,12 +75,12 @@ void *LibJITFormula::emit (BinaryExprAST *expr)
 		// Return value
 		return val;
 	}
-		
+
 	// Generate both sides
 	jit_value_t L = (jit_value_t)expr->LHS->generate (this);
 	jit_value_t R = (jit_value_t)expr->RHS->generate (this);
 	if (L == NULL || R == NULL) return NULL;
-	
+
 	if (expr->op == "<=")
 		return jit_insn_le (function, L, R);
 	else if (expr->op == ">=")
@@ -113,9 +113,10 @@ void *LibJITFormula::emit (BinaryExprAST *expr)
 		jit_value_t args[2];
 		args[0] = L;
 		args[1] = R;
-				
-		return jit_insn_call_native (function, "pow", (void *)pow, signature,
-		                             args, 2, 0);
+
+		return jit_insn_call_native (function, "pow",
+                                 (void *)((double (*)(double, double))pow),
+                                 signature, args, 2, 0);
 	}
 	else
 		return NULL;
@@ -128,7 +129,7 @@ void *LibJITFormula::emit (CallExprAST *expr)
 		// We're going to make a new temporary, and store a value to it depending
 		// on what happens here.
 		jit_value_t result = jit_value_create (function, jit_type_float64);
-				
+
 		// if (cond)
 		jit_value_t cond = (jit_value_t)expr->args[0]->generate (this);
 		if (!cond) return NULL;
@@ -138,14 +139,14 @@ void *LibJITFormula::emit (CallExprAST *expr)
 		jit_value_t comparison = jit_insn_eq (function, cond, one);
 		jit_label_t label_if = jit_label_undefined;
 		jit_label_t label_end = jit_label_undefined;
-				
+
 		jit_insn_branch_if_not (function, comparison, &label_if);
 
 		// (the if-value)
 		jit_value_t t = (jit_value_t)expr->args[1]->generate (this);
 		jit_insn_store (function, result, t);
 		jit_insn_branch (function, &label_end);
-		
+
 		// The else branches to here...
 		jit_insn_label (function, &label_if);
 
@@ -154,7 +155,7 @@ void *LibJITFormula::emit (CallExprAST *expr)
 		jit_insn_store (function, result, f);
 
 		jit_insn_label (function, &label_end);
-		
+
 		return result;
 	}
 	else if (expr->function == "sign")
@@ -167,18 +168,18 @@ void *LibJITFormula::emit (CallExprAST *expr)
 		jit_value_t minusone = jit_value_create_float64_constant (function,
 		                                                          jit_type_float64,
 		                                                          -1.0);
-		
+
 		jit_value_t arg = (jit_value_t)expr->args[0]->generate (this);
 		jit_value_t result = jit_value_create (function, jit_type_float64);
 		jit_label_t label_end = jit_label_undefined;
-		
+
 		jit_value_t positive = jit_insn_gt (function, arg, zero);
 		jit_label_t label_positive = jit_label_undefined;
 		jit_insn_branch_if_not (function, positive, &label_positive);
 		jit_insn_store (function, result, one);
 		jit_insn_branch (function, &label_end);
 		jit_insn_label (function, &label_positive);
-		
+
 		jit_value_t negative = jit_insn_lt (function, arg, zero);
 		jit_label_t label_negative = jit_label_undefined;
 		jit_insn_branch_if_not (function, negative, &label_negative);
@@ -197,18 +198,19 @@ void *LibJITFormula::emit (CallExprAST *expr)
         jit_value_t args[2];
         args[0] = (jit_value_t)expr->args[0]->generate (this);
         args[1] = (jit_value_t)expr->args[1]->generate (this);
-        
+
         jit_type_t params[2];
         params[0] = jit_type_float64;
         params[1] = jit_type_float64;
         jit_type_t signature;
         signature = jit_type_create_signature (jit_abi_cdecl, jit_type_float64,
                                                params, 2, 1);
-        
-        return jit_insn_call_native (function, "atan2", (void *)atan2, signature,
-                                     args, 2, 0);
+
+        return jit_insn_call_native (function, "atan2",
+                                     (void *)((double (*)(double, double))atan2),
+                                     signature, args, 2, 0);
 	}
-	
+
 	// Compile the arguments
 	// HACK: we just assume for now that if() is the only thing with
 	// more than one argument.
@@ -225,60 +227,77 @@ void *LibJITFormula::emit (CallExprAST *expr)
 	// by jit_insn_call_native, because the intrinsic instructions seem to
 	// give the wrong answers.
 	if (expr->function == "sin")
-		return jit_insn_call_native (function, "sin", (void *)sin, signature,
-		                             &arg, 1, 0);
+		return jit_insn_call_native (function, "sin",
+                                 (void *)((double (*)(double))sin),
+                                 signature, &arg, 1, 0);
 	else if (expr->function == "cos")
-		return jit_insn_call_native (function, "cos", (void *)cos, signature,
-		                             &arg, 1, 0);
+		return jit_insn_call_native (function, "cos",
+                                 (void *)((double (*)(double))cos),
+                                 signature, &arg, 1, 0);
 	else if (expr->function == "tan")
-		return jit_insn_call_native (function, "tan", (void *)tan, signature,
-		                             &arg, 1, 0);
+		return jit_insn_call_native (function, "tan",
+                                 (void *)((double (*)(double))tan),
+                                 signature, &arg, 1, 0);
 	else if (expr->function == "asin")
-		return jit_insn_call_native (function, "asin", (void *)asin, signature,
-		                             &arg, 1, 0);
+		return jit_insn_call_native (function, "asin",
+                                 (void *)((double (*)(double))asin),
+                                 signature, &arg, 1, 0);
 	else if (expr->function == "acos")
-		return jit_insn_call_native (function, "acos", (void *)acos, signature,
-		                             &arg, 1, 0);
+		return jit_insn_call_native (function, "acos",
+                                 (void *)((double (*)(double))acos),
+                                 signature, &arg, 1, 0);
 	else if (expr->function == "atan")
-		return jit_insn_call_native (function, "atan", (void *)atan, signature,
-		                             &arg, 1, 0);
+		return jit_insn_call_native (function, "atan",
+                                 (void *)((double (*)(double))atan),
+                                 signature, &arg, 1, 0);
 	else if (expr->function == "sinh")
-		return jit_insn_call_native (function, "sinh", (void *)sinh, signature,
-		                             &arg, 1, 0);
+		return jit_insn_call_native (function, "sinh",
+                                 (void *)((double (*)(double))sinh),
+                                 signature, &arg, 1, 0);
 	else if (expr->function == "cosh")
-		return jit_insn_call_native (function, "cosh", (void *)cosh, signature,
-		                             &arg, 1, 0);
+		return jit_insn_call_native (function, "cosh",
+                                 (void *)((double (*)(double))cosh),
+                                 signature, &arg, 1, 0);
 	else if (expr->function == "tanh")
-		return jit_insn_call_native (function, "tanh", (void *)tanh, signature,
-		                             &arg, 1, 0);
+		return jit_insn_call_native (function, "tanh",
+                                 (void *)((double (*)(double))tanh),
+                                 signature, &arg, 1, 0);
 	else if (expr->function == "asinh")
-		return jit_insn_call_native (function, "asinh", (void *)asinh, signature,
-		                             &arg, 1, 0);
+		return jit_insn_call_native (function, "asinh",
+                                 (void *)((double (*)(double))asinh),
+                                 signature, &arg, 1, 0);
 	else if (expr->function == "acosh")
-		return jit_insn_call_native (function, "acosh", (void *)acosh, signature,
-		                             &arg, 1, 0);
+		return jit_insn_call_native (function, "acosh",
+                                 (void *)((double (*)(double))acosh),
+                                 signature, &arg, 1, 0);
 	else if (expr->function == "atanh")
-		return jit_insn_call_native (function, "atanh", (void *)atanh, signature,
-		                             &arg, 1, 0);
+		return jit_insn_call_native (function, "atanh",
+                                 (void *)((double (*)(double))atanh),
+                                 signature, &arg, 1, 0);
 	else if (expr->function == "log2")
-		return jit_insn_call_native (function, "log2", (void *)log2, signature,
-		                             &arg, 1, 0);
+		return jit_insn_call_native (function, "log2",
+                                 (void *)((double (*)(double))log2),
+                                 signature, &arg, 1, 0);
 	else if (expr->function == "log" || expr->function == "log10")
-		return jit_insn_call_native (function, "log10", (void *)log10, signature,
-		                             &arg, 1, 0);
+		return jit_insn_call_native (function, "log10",
+                                 (void *)((double (*)(double))log10),
+                                 signature, &arg, 1, 0);
 	else if (expr->function == "ln")
-		return jit_insn_call_native (function, "log", (void *)log, signature,
-		                             &arg, 1, 0);
+		return jit_insn_call_native (function, "log",
+                                 (void *)((double (*)(double))log),
+                                 signature, &arg, 1, 0);
 	else if (expr->function == "exp")
-		return jit_insn_call_native (function, "exp", (void *)exp, signature,
-		                             &arg, 1, 0);
+		return jit_insn_call_native (function, "exp",
+                                 (void *)((double (*)(double))exp),
+                                 signature, &arg, 1, 0);
 	else if (expr->function == "sqrt")
 		return jit_insn_sqrt (function, arg);
 	else if (expr->function == "abs")
 		return jit_insn_abs (function, arg);
 	else if (expr->function == "rint")
-		return jit_insn_call_native (function, "rint", (void *)rint, signature,
-		                             &arg, 1, 0);
+		return jit_insn_call_native (function, "rint",
+                                 (void *)((double (*)(double))rint),
+                                 signature, &arg, 1, 0);
 	else
 		return NULL;
 }
@@ -320,7 +339,7 @@ bool LibJITFormula::buildFunction ()
 		jit_function_abandon (function);
 		return false;
 	}
-		
+
 	return true;
 }
 
